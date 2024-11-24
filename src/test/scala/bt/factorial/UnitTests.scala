b330498ee58c6ececd274f8f1bdf8f01e6b7cb6b
bartek <bondiev@gmail.com>
2024-11-25 00:47:43 +0100

    more tests added

--- src/test/scala/bt/factorial/UnitTests.scala
+++ src/test/scala/bt/factorial/UnitTests.scala
@@ -11,5 +11,16 @@ class UnitTests extends AnyFunSuite {
     assert(factorial(5) == 2 * 3 * 4 * 5, "factorial(5) should be 120")
   }
 
+  test("EvaluateArgs dwadzieścia") {
+    assert(evaluateArgs(Array("jkjkjk"), defaultValue = 20) == 20, "evaluateArgs(\"dwadzieścia\", defaultValue = 20) should be 20")
+  }
+
+  test("EvaluateArgs 200") {
+    assert(evaluateArgs(Array("200"), defaultValue = 20) == 20, "evaluateArgs(\"200\", defaultValue = 20) should be 20")
+  }
+
+  test("EvaluateArgs 5") {
+    assert(evaluateArgs(Array("5"), defaultValue = 20) == 5, "evaluateArgs(\"5\", defaultValue = 20) should be 5")
+  }
 
 }
